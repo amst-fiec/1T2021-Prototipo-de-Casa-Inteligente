936a2c9ddf870a0484ec11b7552b3847053baea7
cjapolo <67070980+cjapolo@users.noreply.github.com>
2021-07-22 12:35:49 -0500

    plantilla_inicio_sesion

---
+++ Smart_Home/app/src/main/java/com/example/smart_home/InicioSesion.java
@@ -0,0 +1,13 @@
+package com.example.smart_home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InicioSesion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_sesion);
+    }
+}
--- Smart_Home/app/src/main/java/com/example/smart_home/MainActivity.java
+++ Smart_Home/app/src/main/java/com/example/smart_home/MainActivity.java
@@ -2,12 +2,23 @@ package com.example.smart_home;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnIniciarS;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnIniciarS = (Button) findViewById(R.id.btnIniciarS);
+    }
+
+    public void IniciarSesion(){
+        Intent intent = new Intent(this, InicioSesion.class);
+        startActivity(intent);
     }
 }
