77e016833ac4ab09e3a3080c30e55ce2c79cfe01
david llerena <dllerena@espol.edu.ec>
2021-07-22 15:40:58 -0500

    Actividad 4 resuelta click para abrir actividad de temperatura
    Actividad 4 resuelta click para abrir actividad de temperatura

--- Smart_Home/app/src/main/java/com/example/smart_home/MainActivity.java
+++ Smart_Home/app/src/main/java/com/example/smart_home/MainActivity.java
@@ -1,13 +1,26 @@
 package com.example.smart_home;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = (Button) findViewById(R.id.btn_temp);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this, MenuActivity.class));
+
+            }
+        });
     }
 }
