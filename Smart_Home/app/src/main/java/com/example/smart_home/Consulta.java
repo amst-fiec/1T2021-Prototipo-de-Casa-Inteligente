24994150bab068cf9852cb0a2e0d81bbd274cc88
Marcelo Mendoza <marjomen@espol.edu.ec>
2021-07-22 17:31:09 -0500

    Acitivity y vista de Consulta

---
+++ Smart_Home/app/src/main/java/com/example/smart_home/Consulta.java
@@ -0,0 +1,13 @@
+package com.example.smart_home;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Consulta extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_consulta);
+    }
+}
