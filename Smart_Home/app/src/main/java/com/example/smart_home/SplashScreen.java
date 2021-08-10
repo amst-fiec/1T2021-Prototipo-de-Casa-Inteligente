30a81eef0e2758e370e8bb4353608117e9b4ede1
cjapolo <67070980+cjapolo@users.noreply.github.com>
2021-08-10 01:20:26 -0500

    apolo
    arreglo de vistas, arreglo inicio sesion

---
+++ Smart_Home/app/src/main/java/com/example/smart_home/SplashScreen.java
@@ -0,0 +1,32 @@
+package com.example.smart_home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.os.Handler;
+import android.content.Intent;
+import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        TimerTask tarea = new TimerTask(){
+
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class );
+                startActivity(intent);
+                finish();
+            }
+        };
+        Timer tiempo = new Timer();
+        tiempo.schedule(tarea, 3000);
+    }
+}
