c19a34cc0d96b32dd84aeff7f2ae7a4aff16454c
cjapolo <67070980+cjapolo@users.noreply.github.com>
2021-08-18 01:47:04 -0500

    v3
    Mejora interfaz, lectura de base

--- Smart_Home/app/src/main/java/com/example/smart_home/Estado_Hogar.java
+++ Smart_Home/app/src/main/java/com/example/smart_home/Estado_Hogar.java
@@ -1,13 +1,21 @@
 package com.example.smart_home;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -19,108 +27,135 @@ public class Estado_Hogar extends AppCompatActivity {
 
     DatabaseReference db_reference;
 
-    TextView tv_temp, tv_hume, tv_sound, tv_dht11, tv_gas;
+    TextView tv_temp, tv_hume, tv_sound, tv_gas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_estado_hogar);
-        db_reference = FirebaseDatabase.getInstance().getReference().child("lecturas");
-        //iniciarBaseDeDatos();
+        //Instancia de la base
+        db_reference = FirebaseDatabase.getInstance().getReference();
         leer_sensores();
     }
 
     //Funcion para hacer la lectura a la base
     public void leer_sensores(){
-
-        db_reference.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                    //mostrarLecturasGasSound(snapshot);
-                    det_confort(snapshot);
+            db_reference.addValueEventListener( new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                    try {
+                        String temp = String.valueOf(dataSnapshot.child("lecturas").child("temp").getValue());
+                        String humed = String.valueOf(dataSnapshot.child("lecturas").child("humed").getValue());
+                        String gas = String.valueOf(dataSnapshot.child("lecturas").child("gas").getValue());
+                        String sound = String.valueOf(dataSnapshot.child("lecturas").child("sound").getValue());
+                        if(!temp.isEmpty() && !humed.isEmpty()&&!gas.isEmpty() && !sound.isEmpty()){
+                            det_confort(Double.parseDouble(temp),Double.parseDouble(humed));
+                            mostrarLecturasGasSound(Double.parseDouble(sound),Double.parseDouble(gas));
+                        }
+                    } catch (NullPointerException n){
+                        toaster("Error al obtener los valores");
+                    }
                 }
-            }
-            @Override
-            public void onCancelled(DatabaseError error) {
-                System.out.println(error.toException().toString()); //El error puede no ser un String
-            }
-        });
-    }
-
-    public void iniciarBaseDeDatos(){
-        db_reference = FirebaseDatabase.getInstance().getReference().child("Lecturas");
-    }
-    //Funcion para mostrar parametros de los sensores de gas y sonido
-    public void mostrarLecturasGasSound(DataSnapshot snapshot){
-
-        String aux_sound, aux_gas;
-
-        aux_sound = String.valueOf(snapshot.child("Lecturas").child("KY38").child("level_s").getValue());
-        aux_gas = String.valueOf(snapshot.child("Lecturas").child("MQ2").child("level_g").getValue());
-
-        tv_sound = findViewById(R.id.tv_sound);
-        tv_gas = findViewById(R.id.tv_gas);
-
-        tv_sound.setText(aux_sound);
-        tv_gas.setText(aux_gas);
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    System.out.println(error.toException().toString()); //El error puede no ser un String
+                }
+            });
     }
 
     //HU2 - Act4
     //Función para determinar parametros de confort
-    public void det_confort(DataSnapshot snapshot){
+    public void det_confort(Double t, Double h){
 
-        float aux_temp, aux_humed;
-        /*aux_temp = (float) snapshot.child("Lecturas").child("DHT11").child("temp").getValue();
-        aux_humed = (float) snapshot.child("Lecturas").child("DHT11").child("humed").getValue();
+        if (t!=0.0){//Evaluar que el valor no sea 0, caso contrario notificar
+            tv_temp = findViewById(R.id.tv_temp);
+            String estat_t ="n/a";
 
-         */
-
-        aux_temp = (float) snapshot.child("lecturas").child("temp").getValue();
-        aux_humed = (float) snapshot.child("lecturas").child("humed").getValue();
-
-        tv_temp = findViewById(R.id.tv_temp);
-        tv_hume = findViewById(R.id.tv_hume);
-        String estat_t ="n/a", estat_h = "n/a";
-
-        if(aux_temp > 27 ){
-            estat_t = "Peligro - Sofocante";
-        } else if(aux_temp < 27 && aux_temp >25){
-            estat_t = "Caliente";
-        }else if(aux_temp < 25 && aux_temp >24){
-            estat_t = "Confortable";
-        }else if(aux_temp < 24 && aux_temp >22){
-            estat_t = "Frio";
-        }else if(aux_temp < 22 ){
-            estat_t = "Peligro - Muy frio";
+            if(t > 27 ){
+                estat_t = "Peligro - Sofocante";
+            } else if(t >25 && t < 27){
+                estat_t = "Caliente";
+            }else if(t >24 && t < 25){
+                estat_t = "Confortable";
+            }else if(t >22 && t < 24){
+                estat_t = "Frio";
+            }else if(t < 22 ){
+                estat_t = "Peligro - Muy frio";
+            }
+            tv_temp.setText(estat_t);
+        }else {
+            Toast.makeText(getApplicationContext(), "Valores en 0, revise el dispositivo",Toast.LENGTH_SHORT).show();
         }
-
-        if(aux_humed >= 50 ){
-            estat_h = "Peligro - Electricidad Estatica";
-        } else if(aux_humed >= 33 && aux_humed <40){
-            estat_h = "Confortable";
+        if (h!=0.0){        //Evaluar que el valor no sea 0, caso contrario notificar
+            tv_hume = findViewById(R.id.tv_hume);
+            String estat_h = "n/a";
+            if(h >= 50 ){
+                estat_h = "Peligro - Electricidad Estatica";
+            } else if(h >= 33 && h <40){
+                estat_h = "Confortable";
+            }
+            tv_hume.setText(estat_h);
+        }else {
+            toaster("Valores en 0, revise el dispositivo");
         }
+    }
 
-        tv_temp.setText(estat_t);
-        tv_hume.setText(estat_h);
+    //Funcion para mostrar parametros de los sensores de gas y sonido
+    public void mostrarLecturasGasSound(Double s, Double g){
+        if (s!=0.0){//Evaluar que el valor no sea 0, caso contrario notificar
+            String aux_sound = String.valueOf(s);
+            tv_sound = findViewById(R.id.tv_sound);
+            tv_sound.setText(aux_sound);
+        }else {
+            toaster("Valores en 0, revise el dispositivo");
+        }
+        if (g!=0.0){//Evaluar que el valor no sea 0, caso contrario notificar
+            String aux_gas = String.valueOf(g);
+            tv_gas = findViewById(R.id.tv_gas);
+            tv_gas.setText(aux_gas);
+        }else {
+            toaster("Valores en 0, revise el dispositivo");
+        }
     }
 
     //HU2 - Act5
     //Función para determinar fuga de gas
-    public void eval_Gas(){
+    public void eval_Gas(Double g){
+        if (g!=0.0){//Evaluar que el valor no sea 0, caso contrario notificar
+            tv_temp = findViewById(R.id.tv_temp);
+            String estat_gas_level ="n/a";
 
+            if(g > 159 ){
+                estat_gas_level = "Gas detectado";
+                toaster("Peligro de intoxicacion");
+            } else if(g >90 && g < 159){
+                estat_gas_level = "Gas detectado";
+                toaster("Peligro de incendio");
+            }else if(g >50 && g < 90){
+                estat_gas_level = "Nivel de gas al límite";
+                toaster("Peligro de incendio");
+            }else if(g < 50){
+                estat_gas_level = "Nivel normal";
+            }
+            //Agregar text view para mostrar nivel de gas
+            //tv_temp.setText(estat_gas_level);
+        }else {
+            toaster("Valores en 0, revise el dispositivo");
+        }
     }
 
     //HU2 - Act6
     //Función para determinar avería en el desague
-    public void eval_Tuberia(DataSnapshot snapshot){
-        float aux_sound;
-        aux_sound = (float) snapshot.child("Lecturas").child("KY38").child("level_s").getValue();
-            if(aux_sound < 400){
-                String titulo = "Obstruccion";
-                String alerta = "Se ha detectado una tuberia tapada";
-                int n_id = 1;
-                notificarFallo(titulo, alerta, n_id);
+    public void eval_Tuberia(Double g){
+            if(g!=0.0){
+                if(g < 400) {
+                    String titulo = "Obstruccion";
+                    String alerta = "Se ha detectado una tuberia tapada";
+                    int n_id = 1;
+                    notificarFallo(titulo, alerta, n_id);
+                }
+            }else {
+                toaster("Valores en 0, revise el dispositivo");
             }
     }
 
@@ -135,11 +170,28 @@ public class Estado_Hogar extends AppCompatActivity {
                 .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
         NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
-
         // notificationId is a unique int for each notification that you must define
         notificationManager.notify(n, builder.build());
     }
 
+    //Permisos de CONEXION INTERNET - LOCALIZACIÓN
+    public boolean verConexioInternet() {
+        try {
+            ConnectivityManager con = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+            assert con != null;
+            NetworkInfo networkInfo = con.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isConnected()) {
+                return true;
+            } else {
+                toaster("Verifique su conexion de Internet");
+                return false;
+            }
+        } catch (NullPointerException n) {
+            return false;
+        }
+    }
 
-
+    public void toaster(String msg){
+        Toast.makeText(getApplicationContext(), msg,Toast.LENGTH_SHORT).show();
+    }
 }
